d4f408b1375217366513814426d69949b40d6ec3
kimjongmo <kfmd1008@naver.com>
2018-09-11 22:10:11 +0900

    board 작업

--- contract/src/main/java/com/boot/contract/controller/BoardController.java
+++ contract/src/main/java/com/boot/contract/controller/BoardController.java
@@ -1,13 +1,29 @@
 package com.boot.contract.controller;
 
+import com.boot.contract.model.Board;
 import com.boot.contract.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
     @Autowired
     private BoardService boardService;
+
+
+
+    @RequestMapping("/list")
+    public String list(Model model){
+
+        List<Board> list = boardService.getList();
+        model.addAttribute("board",list);
+
+        return "board";
+    }
 }
--- contract/src/main/java/com/boot/contract/service/BoardService.java
+++ contract/src/main/java/com/boot/contract/service/BoardService.java
@@ -1,11 +1,18 @@
 package com.boot.contract.service;
 
+import com.boot.contract.model.Board;
 import com.boot.contract.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BoardService {
     @Autowired
     private BoardRepository boardRepository;
+
+    public List<Board> getList(){
+        return boardRepository.findAll();
+    }
 }
